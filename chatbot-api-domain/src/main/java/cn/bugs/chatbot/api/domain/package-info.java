c0f4b5d1caf061091a8257e98a0437943a0a0976
sun-haomiao <sunhaomiao111@gmail.com>
2023-06-19 15:55:49 +0800

    shm  feat: init ddd project

---
+++ chatbot-api-domain/src/main/java/cn/bugs/chatbot/api/domain/package-info.java
@@ -0,0 +1,1 @@
+package cn.bugs.chatbot.api.domain;
---
+++ chatbot-api-interfaces/src/main/java/cn/bugs/chatbot/api/ApiApplication.java
@@ -0,0 +1,12 @@
+package cn.bugs.chatbot.api;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot. SpringApplication;
+//启动类
+
+@SpringBootApplication
+public class ApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+}
